b0d2122eb37e9aca7645ef5b8b1173201ff94c8c
Benjamin Pereto <benjamin@sandchaschte.ch>
2019-01-05 18:00:29 +0100

    FEATURE: update libacl to version 2.2.53 and fix build

--- packages/libacl/build.sh
+++ packages/libacl/build.sh
@@ -1,14 +1,13 @@
 TERMUX_PKG_HOMEPAGE=http://savannah.nongnu.org/projects/acl
 TERMUX_PKG_DESCRIPTION="Access control list shared library"
-TERMUX_PKG_VERSION=2.2.52
+TERMUX_PKG_VERSION=2.2.53
 TERMUX_PKG_MAINTAINER="Oliver Schmidhauser @Neo-Oli"
-TERMUX_PKG_SRCURL=https://download.savannah.gnu.org/releases/acl/acl-${TERMUX_PKG_VERSION}.src.tar.gz
-TERMUX_PKG_SHA256=179074bb0580c06c4b4137be4c5a92a701583277967acdb5546043c7874e0d23
+TERMUX_PKG_SRCURL=https://download.savannah.gnu.org/releases/acl/acl-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=06be9865c6f418d851ff4494e12406568353b891ffe1f596b34693c387af26c7
 TERMUX_PKG_FOLDERNAME=acl-$TERMUX_PKG_VERSION
 TERMUX_PKG_DEPENDS="attr"
 TERMUX_PKG_BUILD_IN_SRC=yes
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-gettext=no"
-TERMUX_PKG_MAKE_INSTALL_TARGET="install install-lib install-dev"
 
 #termux_step_pre_configure() {
     #export LDFLAGS+=-lintl
